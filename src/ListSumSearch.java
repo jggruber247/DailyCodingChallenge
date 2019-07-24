4b1ef63c6b9859c39ad2b6fe4d4c373d36e6b1d7
jggruber247 <jggruber247@gmail.com>
2019-07-24 13:37:09 -0400

    Coding

--- src/ListSumSearch.java
+++ src/ListSumSearch.java
@@ -4,7 +4,7 @@
  * July 24, 2019
  *
  * PROBLEM:
- * Given a list of numbers and a number k, return whether 
+ * Given a list of 5 numbers and a number k, return whether 
  * any two numbers from the list add up to k. For example, 
  * given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
  * ---------------------------------------------------------
@@ -12,8 +12,29 @@
 import java.util.Scanner;
 
 public class ListSumSearch {
-	Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
 	
-	
-	sc.close();
+		Scanner sc = new Scanner(System.in);
+		int num1, num2, num3, num4, num5, k;
+		System.out.println("Enter 5 integers seperated by spaces.");
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		num3 = sc.nextInt();
+		num4 = sc.nextInt();
+		num5 = sc.nextInt();
+		System.out.println("Your List: [" + num1 + ", " + num2 + ", " + num3 + ", " + num4 + ", " + num5 + "]");
+		System.out.println("Enter k.");
+		k = sc.nextInt();
+		int[] list = {num1,num2,num3,num4,num5};
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				if(i != j) {
+					if(list[i] + list[j] == k) {
+						System.out.println("" + list[i] + " + " + list[j] + " = " + k);
+					}
+				}
+			}
+		}
+		sc.close();
+	}
 }
