f9fbaad2783dca3e9484e772fcff610b52d8ddb0
jggruber247 <jggruber247@gmail.com>
2019-07-24 12:40:48 -0400

    First commit

---
+++ src/MilesToKilos.java
@@ -0,0 +1,38 @@
+/* ---------------------------------------------------------
+ * James Garrett Gruber
+ * Daily Coding Challenge
+ * July 24, 2019
+ *
+ * PROBLEM:
+ * Take an inputed miles value, convert it to kilometers, 
+ * and display the result using printf().
+ * ---------------------------------------------------------
+ */
+
+import java.util.Scanner;
+public class MilesToKilos {
+
+	public static void main(String[] args) {
+		float miles, km;
+		final double KM_RATIO = 1.60935;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// Programmer Information
+		System.out.println("Garrett Gruber, CSCI 1301, Section DA");
+		System.out.println("-------------------------------------"); 
+		System.out.println();
+		
+		// Input the mile value
+		System.out.println("How many miles would you like to convert?");
+		miles = sc.nextFloat();
+		km = (float) (miles * KM_RATIO);   // Converting miles to kilometers
+		
+		// Display the converted value
+		System.out.println();
+		System.out.print(miles);
+		System.out.printf(" miles is equivalent to %2.5f kilometers.%n", km);
+		
+		sc.close();
+	}
+}
