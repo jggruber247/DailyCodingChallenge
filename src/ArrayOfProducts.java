eb85c297e78f9c9cf07c0b882f59cbfcef77a0c7
jggruber247 <jggruber247@gmail.com>
2019-07-26 11:32:42 -0400

    Adding new file

---
+++ src/ArrayOfProducts.java
@@ -0,0 +1,26 @@
+/* ---------------------------------------------------------
+ * James Garrett Gruber
+ * Daily Coding Challenge
+ * July 25, 2019
+ *
+ * PROBLEM:
+ * Given an array of integers, return a new array such that each 
+ * element at index i of the new array is the product of all the 
+ * numbers in the original array except the one at i. For example, 
+ * if our input was [1, 2, 3, 4, 5], the expected output would 
+ * be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the 
+ * expected output would be [2, 3, 6].
+ * ---------------------------------------------------------
+ */
+
+public class ArrayOfProducts {
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
