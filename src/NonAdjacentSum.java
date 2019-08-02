1d917995e5db4e936053d281aa5fe1f46946f329
jggruber247 <jggruber247@gmail.com>
2019-08-02 09:31:10 -0400

    Starting new File

---
+++ src/NonAdjacentSum.java
@@ -0,0 +1,24 @@
+/* ---------------------------------------------------------
+ * James Garrett Gruber
+ * Daily Coding Challenge
+ * August 2, 2019
+ *
+ * PROBLEM:
+ * Given a list of integers, write a function that returns the
+ * largest sum of non-adjacent numbers. Numbers can be 0 or 
+ * negative. 
+ * For example, [2, 4, 6, 2, 5] should return 13, since we 
+ * pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we 
+ * pick 5 and 5.
+ * ---------------------------------------------------------
+ */
+import java.util.Arrays;
+
+public class NonAdjacentSum {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
