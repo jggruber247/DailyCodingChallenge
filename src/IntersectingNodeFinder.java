15ed68af91a0a5df20ef8d46689d46660495975d
jggruber247 <jggruber247@gmail.com>
2019-08-16 08:38:01 -0400

    New Files

---
+++ src/IntersectingNodeFinder.java
@@ -0,0 +1,22 @@
+/* ---------------------------------------------------------
+ * James Garrett Gruber
+ * Daily Coding Challenge
+ * August 16, 2019
+ *
+ * PROBLEM:
+ * Given two singly linked lists that intersect at some point, 
+ * find the intersecting node. The lists are non-cyclical. 
+ * For example, given A = 3 -> 7 -> 8 -> 10 and B = 99 -> 1 -> 8 -> 10, 
+ * return the node with value 8. In this example, assume nodes with 
+ * the same value are the exact same node objects.
+ * ---------------------------------------------------------
+ */
+
+public class IntersectingNodeFinder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/MinimumRooms.java
@@ -0,0 +1,21 @@
+/* ---------------------------------------------------------
+ * James Garrett Gruber
+ * Daily Coding Challenge
+ * August 16, 2019
+ *
+ * PROBLEM:
+ * Given an array of time intervals (start, end) for classroom 
+ * lectures (possibly overlapping), find the minimum number of 
+ * rooms required. 
+ * For example, given [(30, 75), (0, 50), (60, 150)], you 
+ * should return 2.
+ * ---------------------------------------------------------
+ */
+public class MinimumRooms {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
