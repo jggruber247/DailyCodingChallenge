842eb7561bcba0427e49ec5d9cc17cc8325ae52a
jggruber247 <jggruber247@gmail.com>
2019-09-25 10:08:16 -0400

    New Files

---
+++ src/PowerSet.java
@@ -0,0 +1,20 @@
+/* ---------------------------------------------------------
+ * James Garrett Gruber
+ * Daily Coding Challenge
+ * September 25, 2019
+ *
+ * PROBLEM:
+ * Write a function that, given a set, generates its power set.
+ * The power set of a set is the set of all its subsets.
+ * For example, given the set {1, 2, 3}, it should return 
+ * {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
+ * ---------------------------------------------------------
+ */
+public class PowerSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
