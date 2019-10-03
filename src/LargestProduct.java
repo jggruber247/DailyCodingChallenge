da5670fe42f16a600818ff866ffee633c956bdb4
jggruber247 <jggruber247@gmail.com>
2019-10-03 12:22:50 -0400

    New File

---
+++ src/LargestProduct.java
@@ -0,0 +1,15 @@
+/* ---------------------------------------------------------
+ * James Garrett Gruber
+ * Daily Coding Challenge
+ * October 3, 2019
+ *
+ * PROBLEM:
+ * Given a list of integers, return the largest product that 
+ * can be made by multiplying any three integers.
+ * For example, if the list is [-10, -10, 5, 2], we should 
+ * return 500, since that's -10 * -10 * 5.
+ * ---------------------------------------------------------
+ */
+public class LargestProduct {
+
+}
